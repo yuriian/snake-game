95febb65a21a0d4e90584da2533d02fa805d402d
yuriian <leagyuan@gmail.com>
2018-06-19 12:00:10 +0300

    used pop and unsift to upated snake

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var cnv = document.getElementById('canvas');
 var ctx = cnv.getContext('2d');
 
-var snake = new Game(10);
-snake.draw();
+var game = new Game(5);
+game.draw();
 
 function Game(length) {
   this.length = length || 3;
@@ -15,15 +15,15 @@ function Game(length) {
     h: 6
   };
 
-  this.position = {
+  this.initPos = {
     x: 20,
-    y: 30
+    y: 50
   };
 
   this.turn = {};
   this.turns = [];
 
-  this.snake = createBody(this.length, this.segmentSize, this.position);
+  this.snake = createBody(this.length, this.segmentSize, this.initPos);
 
   function createBody(length, segmentSize, position) {
     var snake = [];
@@ -53,237 +53,52 @@ function Game(length) {
     return snake;
   }
 
-  this.updatePosition = function() {
-    // if (this.turns.length === 0) {
-    //   updatePos.call(this);
-    // }
-    updatePos2.call(this);
+  this.updateBody = function () {
+    this.snake.pop();
 
-    function updatePos2() {
-      if (this.direction === 'down') {
-        this.snake.forEach(function(segment, index, arr) {
-          for (var i = 0; i < this.turns.length; i++) {
-            if (this.turns[i].prevDirection === 'right') {
-              if (segment.x >= this.turns[i].x) {
-                segment.y += this.speed;
-                segment.x = this.turns[i].x;
-              } else {
-                segment.x += this.speed;
-              }
-            } else if (this.turns[i].prevDirection === 'left') {
-              if (segment.x <= this.turns[i].x) {
-                segment.y += this.speed;
-                segment.x = this.turns[i].x;
-              } else {
-                segment.x -= this.speed;
-              }
-            } else {
-              segment.y += this.speed;
-            }
+    const newSegment = {};
+    const firstBodySegment = this.snake[0];
 
-            if (segment.y > cnv.height) {
-              segment.y = -this.segmentSize.h;
-            }
-          }
-        }, this);
+    if (this.direction === 'down') {
+      newSegment.x = firstBodySegment.x;
+      newSegment.y = firstBodySegment.y + this.segmentSize.h;
+
+      if (newSegment.y > cnv.height) {
+        newSegment.y = -this.segmentSize.h;
       }
-      if (this.direction === 'up') {
-        this.snake.forEach(function(segment) {
-          if (turnItem.prevDirection === 'right') {
-            if (segment.x >= turnItem.x) {
-              segment.y -= this.speed;
-              segment.x = turnItem.x;
-            } else {
-              segment.x += this.speed;
-            }
-          } else if (turnItem.prevDirection === 'left') {
-            if (segment.x <= turnItem.x) {
-              segment.y -= this.speed;
-              segment.x = turnItem.x;
-            } else {
-              segment.x -= this.speed;
-            }
-          } else {
-            // return;
-          }
+    }
+    if (this.direction === 'up') {
+      newSegment.x = firstBodySegment.x;
+      newSegment.y = firstBodySegment.y - this.segmentSize.h;
 
-          if (segment.y < 0) {
-            segment.y = cnv.height + this.segmentSize.h;
-          }
-        }, this);
+      if (newSegment.y < 0) {
+        newSegment.y = cnv.height + this.segmentSize.h;
       }
-      if (this.direction === 'right') {
-        this.snake.forEach(function(segment, index, arr) {
-          for (var i = 0; i < this.turns.length; i++) {
-            if (this.turns[i].prevDirection === 'down') {
-              if (segment.y >= this.turns[i].y) {
-                segment.x += this.speed;
-                segment.y = this.turns[i].y;
+    }
+    if (this.direction === 'right') {
+      newSegment.x = firstBodySegment.x + this.segmentSize.w;
+      newSegment.y = firstBodySegment.y;
 
-                if (index === arr.length - 1) {
-                  this.turns.pop();
-                }
-                break;
-              } else {
-                segment.y += this.speed;
-              }
-            } else if (this.turns[i].prevDirection === 'up') {
-              if (segment.y <= this.turns[i].y) {
-                segment.x += this.speed;
-                segment.y = this.turns[i].y;
-
-                if (index === arr.length - 1) {
-                  this.turns.pop();
-                }
-
-                break;
-              } else {
-                segment.y -= this.speed;
-              }
-            } else {
-              // segment.x += this.speed;
-            }
-
-            if (segment.x > cnv.width) {
-              segment.x = -this.segmentSize.w;
-            }
-          }
-        }, this);
+      if (newSegment.x > cnv.width) {
+        newSegment.x = -this.segmentSize.w;
       }
-      if (this.direction === 'left') {
-        this.snake.forEach(function(segment) {
-          if (turnItem.prevDirection === 'down') {
-            if (segment.y >= turnItem.y) {
-              segment.x -= this.speed;
-              segment.y = turnItem.y;
-            } else {
-              segment.y += this.speed;
-            }
-          } else if (turnItem.prevDirection === 'up') {
-            if (segment.y <= turnItem.y) {
-              segment.x -= this.speed;
-              segment.y = turnItem.y;
-            } else {
-              segment.y -= this.speed;
-            }
-          } else {
-          }
+    }
+    if (this.direction === 'left') {
+      newSegment.x = firstBodySegment.x - this.segmentSize.w;
+      newSegment.y = firstBodySegment.y;
 
-          if (segment.x < 0) {
-            segment.x = cnv.width + this.segmentSize.w;
-          }
-        }, this);
+      if (newSegment.x < 0) {
+        newSegment.x = cnv.width + this.segmentSize.w;
       }
     }
 
-    // this.turns.forEach(function(turnItem) {
-    //   updatePos.call(this, turnItem);
-    // }, this);
+    newSegment.w = this.segmentSize.w;
+    newSegment.h = this.segmentSize.h;
 
-    function updatePos(turnItem) {
-      if (this.direction === 'down') {
-        this.snake.forEach(function(segment) {
-          if (turnItem.prevDirection === 'right') {
-            if (segment.x >= this.turn.x) {
-              segment.y += this.speed;
-              segment.x = turnItem.x;
-            } else {
-              segment.x += this.speed;
-            }
-          } else if (turnItem.prevDirection === 'left') {
-            if (segment.x <= turnItem.x) {
-              segment.y += this.speed;
-              segment.x = turnItem.x;
-            } else {
-              segment.x -= this.speed;
-            }
-          } else {
-            segment.y += this.speed;
-          }
-
-          if (segment.y > cnv.height) {
-            segment.y = -this.segmentSize.h;
-          }
-        }, this);
-      }
-      if (this.direction === 'up') {
-        this.snake.forEach(function(segment) {
-          if (turnItem.prevDirection === 'right') {
-            if (segment.x >= turnItem.x) {
-              segment.y -= this.speed;
-              segment.x = turnItem.x;
-            } else {
-              segment.x += this.speed;
-            }
-          } else if (turnItem.prevDirection === 'left') {
-            if (segment.x <= turnItem.x) {
-              segment.y -= this.speed;
-              segment.x = turnItem.x;
-            } else {
-              segment.x -= this.speed;
-            }
-          } else {
-            // return;
-          }
-
-          if (segment.y < 0) {
-            segment.y = cnv.height + this.segmentSize.h;
-          }
-        }, this);
-      }
-      if (this.direction === 'right') {
-        this.snake.forEach(function(segment, i) {
-          if (turnItem.prevDirection === 'down') {
-            if (segment.y >= turnItem.y) {
-              segment.x += this.speed;
-              segment.y = turnItem.y;
-            } else {
-              segment.y += this.speed;
-            }
-          } else if (turnItem.prevDirection === 'up') {
-            if (segment.y <= turnItem.y) {
-              segment.x += this.speed;
-              segment.y = turnItem.y;
-            } else {
-              segment.y -= this.speed;
-            }
-          } else {
-            // segment.x += this.speed;
-          }
-
-          if (segment.x > cnv.width) {
-            segment.x = -this.segmentSize.w;
-          }
-        }, this);
-      }
-      if (this.direction === 'left') {
-        this.snake.forEach(function(segment) {
-          if (turnItem.prevDirection === 'down') {
-            if (segment.y >= turnItem.y) {
-              segment.x -= this.speed;
-              segment.y = turnItem.y;
-            } else {
-              segment.y += this.speed;
-            }
-          } else if (turnItem.prevDirection === 'up') {
-            if (segment.y <= turnItem.y) {
-              segment.x -= this.speed;
-              segment.y = turnItem.y;
-            } else {
-              segment.y -= this.speed;
-            }
-          } else {
-          }
-
-          if (segment.x < 0) {
-            segment.x = cnv.width + this.segmentSize.w;
-          }
-        }, this);
-      }
-    }
+    this.snake.unshift(newSegment);
   };
 
-  this.setNewDirection = function(newDirection) {
+  this.setNewDirection = function (newDirection) {
     if (
       this.direction === newDirection ||
       (this.direction === 'down' && newDirection === 'up') ||
@@ -305,47 +120,65 @@ function Game(length) {
     this.direction = newDirection;
   };
 
-  this.draw = function() {
+  function updatePosition(segment, speed) {
+    if (this.direction === 'down') {
+      segment.y += speed;
+    }
+    if (this.direction === 'up') {
+      segment.y -= speed;
+    }
+    if (this.direction === 'right') {
+      segment.x += speed;
+    }
+    if (this.direction === 'left') {
+      segment.x -= speed;
+    }
+  }
+
+  this.draw = function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = 'green';
 
-    this.snake.forEach(function(segment, i) {
+    this.snake.forEach(function (segment, i) {
       // ctx.fillRect(segment.x, segment.y, segment.w, segment.h);
       ctx.beginPath();
       ctx.arc(segment.x, segment.y, segment.w / 2, 0, 2 * Math.PI, false);
       ctx.fill();
       // ctx.strokeStyle = "#000";
       // ctx.strokeRect(segment.x - 1, segment.y - 1, segment.w + 2, segment.h + 2);
+
+      // updatePosition(segment, this.speed);
     });
 
-    requestAnimationFrame(this.draw.bind(this));
+    // requestAnimationFrame(this.draw.bind(this));
 
     if (this.isStart) {
-      this.updatePosition();
+      this.updateBody();
     }
   };
 }
+let intervalId = setInterval(game.draw.bind(game), 150);
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
   e = e || window.event;
 
   if (e.keyCode == '37') {
     // left arrow
-    snake.setNewDirection('left');
-    // snake.isStart = true;
+    game.setNewDirection('left');
+    // game.isStart = true;
   } else if (e.keyCode == '38') {
     // up arrow
-    snake.setNewDirection('up');
-    // snake.isStart = true;
+    game.setNewDirection('up');
+    // game.isStart = true;
   } else if (e.keyCode == '39') {
     // right arrow
-    snake.setNewDirection('right');
-    // snake.isStart = true;
+    game.setNewDirection('right');
+    // game.isStart = true;
   } else if (e.keyCode == '40') {
     // down arrow
-    snake.setNewDirection('down');
-    snake.isStart = true;
+    game.setNewDirection('down');
+    game.isStart = true;
   } else {
     return;
   }
